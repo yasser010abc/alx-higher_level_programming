63b45264ccdf5766e1dd972bacc949da6c4e275b
yasser010abc <yassirbenhouria2000@gmail.com>
2024-01-10 04:31:43 -0800

    a

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""0x0B. Python - Input/Output, task 2. Read n lines """
+"""Defines a file-appending function."""
 
 
-def read_lines(filename="", nb_lines=0):
-    """Reads n number of lines of a text file, and prints to stdout.
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file.
 
     Args:
-        filename (str): name of file to be opened
-        nb_lines (int): number of lines to read
-
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
     """
-    line_count = 0
-    with open(filename, encoding='utf-8') as file:
-        for line in file:
-            line_count += 1
-        if nb_lines <= 0 or nb_lines > line_count:
-            nb_lines = line_count
-        file.seek(0)
-        for i in range(nb_lines):
-            print(file.readline(), end='')
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
