39f0118c900306a5e83748e0d4f5a576d8fd0fbe
yasser010abc <yassirbenhouria2000@gmail.com>
2024-03-16 14:34:12 -0700

    a

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,5 +10,4 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(argv[1], argv[2],
                 argv[3]), pool_pre_ping=True)
-#!/usr/bin/python3
     Base.metadata.create_all(engine)
