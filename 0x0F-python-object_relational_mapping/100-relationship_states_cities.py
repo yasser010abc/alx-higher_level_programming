c78f89dd9a1282c796a02b307c9f75481d8d89af
yasser010abc <yassirbenhouria2000@gmail.com>
2024-03-16 14:56:42 -0700

    a

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,23 @@
--- Create the database hbtn_0e_100_usa
-CREATE DATABASE IF NOT EXISTS hbtn_0e_100_usa;
-USE hbtn_0e_100_usa;
+#!/usr/bin/python3
+"""List all states"""
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-SELECT * FROM states;
-SELECT * FROM cities;
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name="California")
+    session.add(new_state)
+    session.commit()
+    new_city = City(name="San Francisco", state_id=new_state.id)
+    session.add(new_city)
+    session.commit()
+    session.close()
